d8d0d91f2173918db97f7a43953b1d7588e2a51a
JakeBrowning90 <jacobbrowning90@gmail.com>
2023-08-02 16:16:25 -0400

    Add new style to project divs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let projectsList = [
-    {"title": "I Spy", "repoLink":"https://github.com/JakeBrowning90/wheres-wally", "liveLink":"https://jakebrowning90.github.io/wheres-wally/", "thumbnail":"img/kiwi.jpg", "desc":"A hidden object game built with JavaScript and an online scoreboard in Cloud Firestore."},
-    {"title": "Memory Card Game", "repoLink":"", "liveLink":"", "thumbnail":"", "desc":""},
-    {"title": "CV Builder", "repoLink":"", "liveLink":"", "thumbnail":"", "desc":""},
-    {"title": "Battleship", "repoLink":"", "liveLink":"", "thumbnail":"", "desc":""},
-    {"title": "Weather App", "repoLink":"", "liveLink":"", "thumbnail":"", "desc":""},
-    {"title": "To-Do List", "repoLink":"", "liveLink":"", "thumbnail":"", "desc":""},
-    {"title": "Tic-Tac-Toe", "repoLink":"", "liveLink":"", "thumbnail":"", "desc":""},
+    {"title": "I Spy", "repoLink":"https://github.com/JakeBrowning90/wheres-wally", "liveLink":"https://jakebrowning90.github.io/wheres-wally/", "thumbnail":"img/ispyThumbnail.jpg", "desc":"A hidden object game built with JavaScript and an online scoreboard in Cloud Firestore."},
+    {"title": "Memory Cards", "repoLink":"", "liveLink":"", "thumbnail":"img/memoryThumbnail.jpg", "desc":""},
+    {"title": "CV Builder", "repoLink":"", "liveLink":"", "thumbnail":"img/cvBuilderThumbnail.jpg", "desc":""},
+    {"title": "Battleship", "repoLink":"", "liveLink":"", "thumbnail":"img/battleshipThumbnail.jpg", "desc":""},
+    {"title": "Weather App", "repoLink":"", "liveLink":"", "thumbnail":"img/weatherThumbnail.jpg", "desc":""},
+    {"title": "To-Do List", "repoLink":"", "liveLink":"", "thumbnail":"img/kiwi.jpg", "desc":""},
+    {"title": "Tic-Tac-Toe", "repoLink":"", "liveLink":"", "thumbnail":"img/tictactoeThumbnail.jpg", "desc":""},
 ];
 
 function showProjects() {
@@ -14,10 +14,15 @@ function showProjects() {
         let projectCard = document.createElement("div");
         projectCard.classList.add("projectCard");
 
+        let projectHeading = document.createElement("div");
+        projectHeading.classList.add("projectHeading");
+
         let projectTitle = document.createElement("h2");
+        projectTitle.classList.add("projectTitle");
         projectTitle.textContent = projectsList[project].title;
 
         let projectDesc = document.createElement("p");
+        projectDesc.classList.add("projectDesc");
         projectDesc.textContent = projectsList[project].desc;
 
         let repoLink = document.createElement("a");
@@ -37,12 +42,17 @@ function showProjects() {
         projectThumbnail.setAttribute("alt", projectsList[project].title + " preview");
         projectThumbnail.classList.add("projectThumbnail");
 
-        projectCard.appendChild(projectTitle);
-        projectCard.appendChild(projectDesc);
-        projectCard.appendChild(repoLink);
-        projectCard.appendChild(liveLink);
         projectCard.appendChild(projectThumbnail);
-
+        projectCard.appendChild(projectHeading);
+        projectHeading.appendChild(projectTitle);
+        projectHeading.appendChild(repoLink);
+        projectHeading.appendChild(liveLink);
+        // projectCard.appendChild(projectTitle);
+        // projectCard.appendChild(repoLink);
+        // projectCard.appendChild(liveLink);
+        projectCard.appendChild(projectDesc);
+       
+        
         projectsDisplay.appendChild(projectCard);
     }
 }
