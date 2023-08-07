932c11dfa6dd7a48f39c68c08bf7177ee514e4d7
JakeBrowning90 <jacobbrowning90@gmail.com>
2023-08-07 12:46:54 -0400

    Add link to live project in thumbnail image

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let projectsList = [
     {"title": "I Spy", "repoLink":"https://github.com/JakeBrowning90/i-spy", "liveLink":"https://jakebrowning90.github.io/i-spy/", "thumbnail":"img/ispyThumbnail.jpg", "desc":"A hidden-object game built with JavaScript and an online scoreboard in Cloud Firestore. Locate every item as fast as possible to join the scoreboard."},
-    {"title": "Memory", "repoLink":"", "liveLink":"", "thumbnail":"img/memoryThumbnail.jpg", "desc":"A React card game. Select all 25 cards without repeating yourself. Be careful: the cards move to new locations after each selection."},
-    {"title": "CV Builder", "repoLink":"", "liveLink":"", "thumbnail":"img/cvBuilderThumbnail.jpg", "desc":"A React app for building and editing a CV. Collapsable forms allow input for a header, education, and work experience components. "},
-    {"title": "Battleship", "repoLink":"", "liveLink":"", "thumbnail":"img/battleshipThumbnail.jpg", "desc":"The classic board game in JavaScript, playable by 1 or 2 players. Place ships manually or automatically, and be the first to sink the opposing fleet."},
-    {"title": "Weather", "repoLink":"", "liveLink":"", "thumbnail":"img/weatherThumbnail.jpg", "desc":"Check the weather in more than 200,000 cities using the OpenWeather API. You can also toggle between metric imperial measurements."},
-    {"title": "To-Do List", "repoLink":"", "liveLink":"", "thumbnail":"img/kiwi.jpg", "desc":"Add, categorize, edit, and delete tasks on a personal list. The list also persists on a user’s machine using the localStorage property."},
-    {"title": "Tic-Tac-Toe", "repoLink":"", "liveLink":"", "thumbnail":"img/tictactoeThumbnail.jpg", "desc":"Play a JavaScript version of the old-fashioned game against your friend."},
+    {"title": "Memory", "repoLink":"https://github.com/JakeBrowning90/memory-cards", "liveLink":"https://jakebrowning90.github.io/memory-cards/", "thumbnail":"img/memoryThumbnail.jpg", "desc":"A React card game. Select all 25 cards without repeating yourself. Be careful: the cards move to new locations after each selection."},
+    {"title": "CV Builder", "repoLink":"https://github.com/JakeBrowning90/cv-project", "liveLink":"https://jakebrowning90.github.io/cv-project/", "thumbnail":"img/cvBuilderThumbnail.jpg", "desc":"A React app for building and editing a CV. Collapsable forms allow input for a header, education, and work experience components. "},
+    {"title": "Battleship", "repoLink":"https://github.com/JakeBrowning90/battleship", "liveLink":"https://jakebrowning90.github.io/battleship/", "thumbnail":"img/battleshipThumbnail.jpg", "desc":"The classic board game in JavaScript, playable by 1 or 2 players. Place ships manually or automatically, and be the first to sink the opposing fleet."},
+    {"title": "Weather", "repoLink":"https://github.com/JakeBrowning90/weather-app", "liveLink":"https://jakebrowning90.github.io/weather-app/", "thumbnail":"img/weatherThumbnail.jpg", "desc":"Check the weather in more than 200,000 cities using the OpenWeather API. You can also toggle between metric imperial measurements."},
+    {"title": "To-Do List", "repoLink":"https://github.com/JakeBrowning90/to-do-list", "liveLink":"https://jakebrowning90.github.io/to-do-list/", "thumbnail":"img/kiwi.jpg", "desc":"Add, categorize, edit, and delete tasks on a personal list. The list also persists on a user’s machine using the localStorage property."},
+    {"title": "Tic-Tac-Toe", "repoLink":"https://github.com/JakeBrowning90/tic-tac-toe", "liveLink":"https://jakebrowning90.github.io/tic-tac-toe/", "thumbnail":"img/tictactoeThumbnail.jpg", "desc":"Play a JavaScript version of the old-fashioned game against your friend."},
 ];
 
 function showProjects() {
@@ -35,19 +35,22 @@ function showProjects() {
         githubLogo.setAttribute("src", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg");
 
 
-
-        // let liveLink = document.createElement("a");
-        // liveLink.setAttribute("href", projectsList[project].liveLink)
-        // liveLink.setAttribute("target", "_blank")
-        // liveLink.setAttribute("rel", "noopener noreferrer")
+        let liveLink = document.createElement("a");
+        liveLink.classList.add("liveLink");
+        liveLink.setAttribute("href", projectsList[project].liveLink)
+        liveLink.setAttribute("target", "_blank")
+        liveLink.setAttribute("rel", "noopener noreferrer")
         // liveLink.textContent = "View live";
 
         let projectThumbnail = document.createElement("img");
         projectThumbnail.setAttribute("src", projectsList[project].thumbnail);
-        projectThumbnail.setAttribute("alt", projectsList[project].title + " preview");
+        projectThumbnail.setAttribute("alt", projectsList[project].title + " link");
         projectThumbnail.classList.add("projectThumbnail");
 
-        projectCard.appendChild(projectThumbnail);
+        // projectCard.appendChild(projectThumbnail);
+        projectCard.appendChild(liveLink);
+        liveLink.appendChild(projectThumbnail);
+
         projectCard.appendChild(projectHeading);
         projectHeading.appendChild(projectTitle);
         projectHeading.appendChild(repoLink);
